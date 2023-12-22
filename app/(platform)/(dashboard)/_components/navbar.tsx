e7fb39662adeb4ff3360c2a9b0e848369bf3c181
LucasNF22 <lucasnfiorentino@gmail.com>
2023-12-22 16:39:34 -0300

    continuando navbar org

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,15 @@
+import { Logo } from "@/components/logo";
+
 export const Navbar = () => {
     return (
-        <div className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+        <nav className="fixed z-50 top-0 px4 w-full h-14 border-b shadow-sm bg-white flex items-center">
             {/* TODO: Mobile Sidebar */}
             <div className="flex items-center gap-x-4">
-        
+                <div className="flex items-center gap-x-4">
+                    <Logo />
+                </div>
             </div>
-        </div>
+        </nav>
     );
 };
 
