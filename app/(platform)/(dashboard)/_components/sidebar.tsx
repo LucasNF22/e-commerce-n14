5b7ac6f9907ca6142593c54e2975d6c7959a679f
LucasNF22 <lucasnfiorentino@gmail.com>
2024-02-03 17:28:09 -0300

    continuando sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { Plus } from 'lucide-react';
 import { useLocalStorage } from 'usehooks-ts';
-import { useOrganization } from '@clerk/nextjs';
+import { useOrganization, useOrganizationList } from '@clerk/nextjs';
 
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
@@ -20,7 +20,34 @@ interface SidebarProps {
 
 export const Sidebar = ( { storageKey = "t-sidebar-state" }: SidebarProps ) => {
     
-    const [ expanded, setExpanded ] = useLocalStorage<Record<string, any>>( storageKey, {} )
+    const [ expanded, setExpanded ] = useLocalStorage<Record<string, any>>( storageKey, {} );
+
+    const { 
+        organization : activeOrganization, 
+        isLoaded: isLoadedOrg 
+    } = useOrganization();
+
+    const {
+        userMemberships,
+        isLoaded: isLoadedOrgList
+    } = useOrganizationList({ userMemberships:{ infinite: true } });
+
+    const defaultAccordionValue: string[] = Object.keys(expanded)
+    .reduce(( acc: string[], key: string ) => {
+        if( expanded[key] ) {
+            acc.push(key)
+        };
+
+        return acc;
+
+    }, []);
+
+    const onExpand = (id: string) => {
+        setExpanded((curr) => ({
+            ...curr, 
+            [id]: !expanded[id]
+        }));
+    };
 
     return(
         <div>
